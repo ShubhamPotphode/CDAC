846467149703f9a3530adbe0c4f21d329a3d771c
ShubhamPotphode <114087331+ShubhamPotphode@users.noreply.github.com>
2024-09-08 14:53:00 +0530

    Add files via upload

---
+++ Assignment/Logic Building Assignment/Assignment 1/Demo.java
@@ -0,0 +1,15 @@
+class Demo{
+   public static void main(String args[]){
+   //System.out.println("Hello world");
+   //System.out.println("Cdac mumbai aug 24");
+
+ System.out.println("Addition of 2 numbers"+(10 + 10));
+
+int n1=10;
+int n2=20;
+int result;
+result = n1 + n2;
+System.out.println("result"+result);
+System.out.println("Addition of 2 numbers is"+ (n1+n2));
+}
+}
---
+++ Assignment/Logic Building Assignment/Assignment 1/OddEven.java
@@ -0,0 +1,11 @@
+class CheckOddOrEven {
+    public static void main(String[] args) {
+        
+        int num = 7; 
+        if (num % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+}
