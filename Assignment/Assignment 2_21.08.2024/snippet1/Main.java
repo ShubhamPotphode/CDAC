07560ae944c8748c93957e56efc158a93a4c9cd6
ShubhamPotphode <114087331+ShubhamPotphode@users.noreply.github.com>
2024-09-08 14:59:52 +0530

    Delete Assignment/Assignment 2_21.08.2024 directory

--- Assignment/Assignment 2_21.08.2024/snippet1/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
- public static void main(String[] args) {
- System.out.println("Hello, World!");
- }
-} 
-/*Error: Main method is not static in class Main, please define the main method as:
-   public static void main(String[] args)
-*/
